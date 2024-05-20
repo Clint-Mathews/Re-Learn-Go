9834579a498da4545a62a9164f343f0874ff3aa3
Clint-Mathews <mathewsclint28@gmail.com>
2024-05-20 07:14:47 +0530

    feat: Added trie implementation

--- dsa/trie/main.go
+++ dsa/trie/main.go
@@ -23,11 +23,44 @@ func InitTrie() *Trie {
 	}
 }
 
-// Insert method
+// Insert method will add a word into the trie
+func (t *Trie) Insert(s string) {
+	currNode := t.root
+	for _, v := range s {
+		charIndex := v - 'a' // Interseting part where we find index by subtracting unicode value of 'a' from the current string
+		if currNode.children[charIndex] == nil {
+			currNode.children[charIndex] = &Node{}
+		}
+		currNode = currNode.children[charIndex]
+	}
+	currNode.isEnd = true
+}
 
-// Serach method
+// Serach method will take a word and return true if it s found in Trie
+func (t *Trie) Search(s string) bool {
+	currNode := t.root
+	for _, v := range s {
+		charIndex := v - 'a'
+		if currNode.children[charIndex] == nil {
+			return false
+		}
+		currNode = currNode.children[charIndex]
+	}
+	return currNode.isEnd
+}
 
 func main() {
 	trie := InitTrie()
-	fmt.Println(trie)
+
+	toAdd := []string{
+		"test", "kkkksd", "popiko",
+	}
+
+	for _, v := range toAdd {
+		trie.Insert(v)
+	}
+
+	fmt.Println("popiko: ", trie.Search("popiko"))
+	fmt.Println("zclint: ", trie.Search("zclint"))
+
 }
